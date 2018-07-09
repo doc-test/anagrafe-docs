f531f5bd77873119974f866b8071a8e4619431af
ninomarrazzo <37708521+ninomarrazzo@users.noreply.github.com>
2018-07-09 12:40:07 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -48,7 +48,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.ifconfig',
-    'docs_italia_theme',
+    'docs-italia-theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
